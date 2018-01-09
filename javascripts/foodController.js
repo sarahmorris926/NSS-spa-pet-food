28e10cfccfc3e37b02ebdde21157ba0d59b4c8e3
Sarah Morris <sarah.morris9@gmail.com>
2018-01-09 15:49:55 -0600

    Removed all comments and console logs

--- javascripts/foodController.js
+++ javascripts/foodController.js
@@ -6,7 +6,6 @@ let foodView = require('./foodView');
 module.exports.outputFood = () => {
   test.fetchDogFood()
   .then( (dogData) => {
-    console.log('dogData where u go?', dogData);
     foodView.displayFood(dogData.dog_brands);
   });
 };
